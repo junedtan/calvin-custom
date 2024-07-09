bd638c504af3d213b282710359286bffb2add599
Server Development Local Juned <server@local.juned.id>
2024-07-09 13:46:47 +0700

    updates

--- parts/company-logo.php
+++ parts/company-logo.php
@@ -5,7 +5,10 @@
 <a href="/">
   <div class="logo">
     <?php 
-      $logo_content = file_get_contents($global_fields['company_logo']['url']);
+      $logo_url = $global_fields['company_logo']['url'];
+      $logo_url = str_replace(get_option('siteurl'),ABSPATH,$logo_url);
+      $logo_url = str_replace('//','/',$logo_url);
+      $logo_content = file_get_contents($logo_url);
       print $logo_content;
     ?>
   </div>
